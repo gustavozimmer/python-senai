fd6be3bf9593554e59adf1d911076e29c5465dc9
Gustavo Zimmer <gustavomzimmer@gmail.com>
2026-02-23 11:05:48 -0300

    INICIAL COMMIT

---
+++ lista_variaveis.py
@@ -0,0 +1,40 @@
+# 1--
+# PI = 3.14159
+# print(PI)
+
+# ---------------
+
+# 2--
+# nome = str(input('Digite sue nome: '))
+# print(f'Seu nome é {nome}')
+
+# ---------------
+
+# 3--
+# primeiro_nome = str(input('Digite seu primeiro nome: '))
+# sobrenome = str(input('Digite seu sobrenome: '))
+# nome_completo = f'{primeiro_nome} {sobrenome}'
+# print(f'Seu nome completo é {nome_completo}')
+
+# ---------------
+
+# 4--
+# a = int(input("Digite um valor: "))
+# b = int(input("Digite outro valor: "))
+# print('Antes da troca:', a, b)
+# a, b = b, a
+# print('Depois da troca:', a, b)
+
+# ---------------
+
+# 5--
+# saldo = float(input("Digite o valor do saldo: "))
+# divida = float(input("Digite o valor da divida: "))
+# print(f'Saldo: {saldo} Divida: {divida}')
+
+# ---------------
+
+# 6--
+# nome = str(input('Digite o nome: '))
+# idade = int(input('Digite sua idade: '))
+# print(f'Olá {nome}, voce tem {idade} anos de idade que legal :)')
---
+++ prática.py
@@ -0,0 +1,46 @@
+# 1--
+# idade = 19
+# nome = 'Gustavo'
+# altura = 1.78
+#
+# print("idade", idade)
+# print("nome", nome)
+# print("altura", altura)
+# -------------------
+
+# 2--
+# nome = "Gustavo"
+# idade = 19
+# print('Olá, meu nome é', nome, 'e tenho', idade, 'anos.')
+# print(f'Olá, meu nome é {nome}, e tenho {idade} anos.')
+# -------------------
+
+# 3--
+# nome1 = "aula"
+# nome2 = "Python"
+# nome3 = nome1
+# nome1 = nome2
+# nome2 = nome3
+# print(nome1)
+# print(nome2)
+# -------------------
+
+#4--
+# nome = str(input("Digite seu nome: "))
+# sobrenome = str(input("Digite seu sobrenome: "))
+# print(nome, sobrenome)
+
+# 5--
+# n = 1
+# n2 = float(n)
+# print(n2)
+# -------------------
+
+# 6--
+# dia = int(input("dia: "))
+# mes = int(input("mes: "))
+# ano = int(input("ano: "))
+# data = f'{dia:02d}/{mes:02d}/{ano}'
+# print(data)
+# -------------------
+
