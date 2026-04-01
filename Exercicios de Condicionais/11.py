826e116dca8e57f6012a43bf553ea369b56c7d13
Gustavo Zimmer <gustavomzimmer@gmail.com>
2026-04-01 11:23:07 -0300

    inicio 11

---
+++ Exercicios de Condicionais/11.py
@@ -0,0 +1,9 @@
+retangulo1_superior_x = int(input("Digite a coordenada x do canto superior esquerdo do retangulo 1: "))
+retangulo1_superior_y = int(input("Digite a coordenada y do canto superior esquerdo do retangulo 1: "))
+retangulo1_inferior_x = int(input("Digite a coordenada x do canto inferior direito do retangulo 1: "))
+retangulo1_inferior_y = int(input("Digite a coordenada y do canto inferior direito do retangulo 1: "))
+
+retangulo2_superior_x = int(input("Digite a coordenada x do canto superior esquerdo do retangulo 2: "))
+retangulo2_superior_y = int(input("Digite a coordenada y do canto superior esquerdo do retangulo 2: "))
+retangulo2_inferior_x = int(input("Digite a coordenada x do canto inferior direito do retangulo 2: "))
+retangulo2_inferior_y = int(input("Digite a coordenada y do canto inferior direito do retangulo 2: "))
