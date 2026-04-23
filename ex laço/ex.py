05e1703641586f443a007fdd480bdad8bc5f095f
Gustavo Zimmer <gustavomzimmer@gmail.com>
2026-04-23 16:51:22 -0300

    atualização ex laco

--- ex laço/ex.py
+++ ex laço/ex.py
@@ -1,20 +1,24 @@
-import sys
-usuario = "admin"
-senha = "1234"
-tentativa = 0
-decisao = 0
+# import sys
+# usuario = "admin"
+# senha = "1234"
+# tentativa = 0
+# decisao = 0
 produtos = []
-while True:
-    tentativa_usuario = str(input("Digite seu usuário: "))
-    tentativa_senha = str(input("Digite sua senha: "))
-    tentativa += 1
-    if tentativa_senha == senha and tentativa_usuario == usuario:
-        print("Bem vindo")
-        break
-    if tentativa == 3:
-        print("Acesso negado")
-        sys.exit()
-    print(f"Tentativas restantes: {3 - tentativa}")
+historico_produto = []
+historico_qtd = []
+historico_valor = []
+zero = False
+# while True:
+#     tentativa_usuario = str(input("Digite seu usuário: "))
+#     tentativa_senha = str(input("Digite sua senha: "))
+#     tentativa += 1
+#     if tentativa_senha == senha and tentativa_usuario == usuario:
+#         print("Bem vindo")
+#         break
+#     if tentativa == 3:
+#         print("Acesso negado")
+#         sys.exit()
+#     print(f"Tentativas restantes: {3 - tentativa}")
 
 while True:
     print("""1. Cadastrar produto
@@ -47,7 +51,7 @@ while True:
         nome = str(input("Digite o nome do produto: "))
         while True: 
             try:
-                preco = float(input("Digite "))
+                preco = float(input("Digite o preço: "))
                 if preco < 0:
                     raise ValueError
                 break
@@ -55,9 +59,119 @@ while True:
                 print("Digite um numero positivo!")
         while True:
             try:
-                quantidade = int(input("Quantidade: "))        
-                if quantidade < 0:
+                estoque = int(input("Quantidade: "))        
+                if estoque < 0:
                     raise ValueError
                 break
             except:
                 print("Digite um numero inteiro positivo!")
+        tipo_produto = [codigo, nome, preco, estoque]
+        produtos.append(tipo_produto)
+    if decisao == "2":
+        if not produtos:
+            print("Nenhum produto adicionado")
+        else:
+            print()
+            for produto in produtos:
+                print(f"{produto[0]} | {produto[1]} | {produto[2]} | {produto[3]}")
+            print()
+    if decisao == "3":
+        print("""1. Adicionar estoque
+2. Remover estoque""")
+        while True:
+            if not produtos:
+                print("Nenhum produto adicionado")
+                zero = True
+                break
+            escolha_estoque = input("Digite sua escolha: ")
+            if escolha_estoque not in ('1', '2'):
+                print("Escolha 1 ou 2")
+            else:
+                print()
+                for produto in produtos:
+                    print(f"{produto[0]} | {produto[1]} | {produto[2]} | {produto[3]}")
+                print()
+                break
+        if zero:
+            print()
+            continue
+        zero = False
+        while True:
+                try:
+                    codigo_pesquisa = int(input("Digite o codigo do produto desejado: "))
+                    if codigo_pesquisa < 0:
+                        raise ValueError
+                    for produto in produtos:
+                        if produto[0] != codigo_pesquisa:    
+                            raise ValueError
+                        else:
+                            indice = produtos.index(produto)
+                            break
+                    break
+                except ValueError:
+                    print("Digite um número válido")
+        if escolha_estoque == "1":
+            while True: 
+                try:
+                    adicao = int(input("Adicione ao estoque: "))
+                    if adicao < 0:
+                        raise ValueError
+                except ValueError:
+                    print("Digite um numero positivo")
+                break
+            produtos[indice][3] += adicao
+            print("Produto adicionado com sucesso!")
+        
+        if escolha_estoque == "2":
+            while True: 
+                try:
+                    remocao = int(input("Remova do estoque: "))
+                    if remocao < 0 or remocao > produtos[indice][3]:
+                        raise ValueError
+                    break
+                except ValueError:
+                    print("Digite um numero menor ou igual a quantidade do estoque")
+            produtos[indice][3] -= remocao
+            print("Produto removido com sucesso")          
+    
+    if decisao == '4':
+        while True:
+            if not produtos:
+                print("Nenhum produto adicionado")
+                break
+            else:
+                print()
+                for produto in produtos:
+                    print(f"{produto[0]} | {produto[1]} | {produto[2]} | {produto[3]}")
+                print()
+                break
+        while True:
+                try:
+                    codigo_pesquisa = int(input("Digite o codigo do produto desejado: "))
+                    if codigo_pesquisa < 0:
+                        raise ValueError
+                    for produto in produtos:
+                        if produto[0] != codigo_pesquisa:    
+                            raise ValueError
+                        else:
+                            indice = produtos.index(produto)
+                            break
+                    break
+                except ValueError:
+                    print("Digite um produto válido")
+        while True: 
+            try:
+                remocao = int(input("Remova do estoque: "))
+                if remocao < 0 or remocao > produtos[indice][3]:
+                    raise ValueError
+                break
+            except ValueError:
+                    print("Digite um numero menor ou igual a quantidade do estoque")
+        produtos[indice][3] -= remocao
+        preço_total = remocao * produtos[indice][2]
+        historico_produto.append(produtos[indice][1])
+        historico_qtd.append(remocao)
+        historico_valor.append(preço_total)
+        print(historico_produto, historico_qtd, historico_valor)
+
+                
